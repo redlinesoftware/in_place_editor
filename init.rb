b8e32c4434abcaa7f5ceae2c7da61b22ca3d91c6
Andrew Kaspick <andrew@redlinesoftware.com>
2007-09-17 16:28:43 +0000

    fix was not correct for edge... load_plugin doesn't exist in edge

--- init.rb
+++ init.rb
@@ -1,5 +1,1 @@
-# if using edge (or rails 2), make sure we load the 'in_place_editing' plugin first
-dependency_path = File.join(File.dirname(__FILE__), '..', 'in_place_editing')
-load_plugin dependency_path if File.exists?(dependency_path)
-
 require 'in_place_editor'
