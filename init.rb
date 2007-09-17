39494e8f91c99db72b95adbd7c36f7b96b4b419e
Andrew Kaspick <andrew@redlinesoftware.com>
2007-09-17 15:22:29 +0000

    add a fix to load the in_place_editing plugin in edge (or rails 2) before we load this plugin

--- init.rb
+++ init.rb
@@ -1,1 +1,5 @@
+# if using edge (or rails 2), make sure we load the 'in_place_editing' plugin first
+dependency_path = File.join(File.dirname(__FILE__), '..', 'in_place_editing')
+load_plugin dependency_path if File.exists?(dependency_path)
+
 require 'in_place_editor'
